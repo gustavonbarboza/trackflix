7e4540a57e2b79034b801e77ef704715c0f66e6e
Gustavo Barboza <gustavobarboza@MacBook-Air-de-Gustavo.local>
2025-05-12 12:36:22 -0300

    Adicionado um layout com o styled components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
+import MovieCard from "../components/MovieCard";
+import styled from "styled-components";
+
+const Grid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-top: 20px;
+`;
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
@@ -15,12 +29,12 @@ function Home() {
 
   return (
     <div>
-      <h1>Filmes Populares</h1>
-      <ul>
-        {filmes.map((filme) => (
-          <li key={filme.id}>{filme.title}</li>
-        ))}
-      </ul>
+      <Title>Filmes Populares</Title>
+      <Grid>
+          {filmes.map((filme) => (
+            <MovieCard key={filmes.id} filme={filme}/>
+          ))};
+      </Grid>
     </div>
   );
 }
