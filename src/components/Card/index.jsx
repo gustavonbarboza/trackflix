51436bfbd0aa6a1665a8a3709ac8f004c3abccb9
Gustavo Barboza <gustavobarboza@MacBook-Air-de-Gustavo.local>
2025-05-14 17:19:35 -0300

    Mudança na imagem dos cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,20 @@
 import { Card, Poster, Title } from "./styles";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import semImagem from "../.././assets/images/sem-imagem.png"
 
 function CardSections({ filme, tipo = "movie" }) {
   return (
     <Link to={`/${tipo}/${filme.id}`} style={{ textDecoration: "none" }}>
       <Card>
         <Poster
-          src={`https://image.tmdb.org/t/p/w300${
-            tipo === "person" ? filme.profile_path : filme.poster_path
-           }`}
+          src={
+            (tipo === "person" && filme.profile_path) || (tipo !== "person" && filme.poster_path)
+              ? `https://image.tmdb.org/t/p/w300${
+                  tipo === "person" ? filme.profile_path : filme.poster_path
+                }`
+              : semImagem
+          }
           alt={filme.title || filme.name}
         />
         <Title>{filme.title || filme.name}</Title>
