f6df1679b59280d756169518ab5740363eda960d
Gustavo Barboza <gustabarboza37@gmail.com>
2025-05-19 17:40:02 -0300

    Adicionando o react-scripts para configuração .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
